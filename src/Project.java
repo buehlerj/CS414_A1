60a0c89c985603cff4fdb30020c72989792968dc
Jeffrey Buehler <buehlerj@rams.colostate.edu>
2016-09-04 16:31:46 -0600

    method structures

--- src/Project.java
+++ src/Project.java
@@ -1,9 +1,53 @@
 
 public class Project {
+	String name;
+	ProjectSize size;
+	ProjectStatus status;
+
+	Project (String name, ProjectSize size, ProjectStatus status) {
+		System.out.println("UNIMPLEMENTED");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
+	public static String getName () {
+		System.out.println("UNIMPLEMENTED");
+		return null;
+	}
+
+	public static ProjectSize getSize () {
+		System.out.println("UNIMPLEMENTED");
+		return null;
+	}
+
+	public static ProjectStatus getStatus () {
+		System.out.println("UNIMPLEMENTED");
+		return null;
+	}
+
+	public static void setStatus (ProjectStatus status) {
+		System.out.println("UNIMPLEMENTED");
+	}
+
+	public static boolean equals () {
+		System.out.println("UNIMPLEMENTED");
+		return true;
+	}
+
+	public String toString () {
+		System.out.println("UNIMPLEMENTED");
+		return null;
+	}
+
+	public static Qualification[] missingQualifications () {
+		System.out.println("UNIMPLEMENTED");
+		return null;
+	}
+
+	public static boolean isHelpful (Worker w) {
+		System.out.println("UNIMPLEMENTED");
+		return true;
+	}
 }
