7a04e96840c91180b920a86603ca37ca45029b53
Jeffrey Buehler <buehlerj@rams.colostate.edu>
2016-09-04 19:09:56 -0600

    Associative Methods

--- src/Company.java
+++ src/Company.java
@@ -22,9 +22,10 @@ public class Company {
 		return assignedWorkerPool;
 	}
 
-	public Worker[] getUnassignedWorkers () {
-		System.out.println("UNIMPLEMENTED");
-		return null;
+	public ArrayList<Worker> getUnassignedWorkers () {
+		ArrayList<Worker> unassigned_workers = new ArrayList<Worker> (availableWorkerPool);
+		unassigned_workers.removeAll(assignedWorkerPool);
+		return unassigned_workers;
 	}
 
 	public boolean equals () {
@@ -42,40 +43,90 @@ public class Company {
 	}
 
 	public void assign (Worker w, Project p) {
-		System.out.println("UNIMPLEMENTED");
+		if (!availableWorkerPool.contains(w)) {
+			System.out.println("Worker not available");
+		}
+		else if (p.getStatus() != ProjectStatus.ACTIVE || p.getStatus() != ProjectStatus.FINISHED) {
+			System.out.println("This project is already " + p.getStatus());
+		}
+		else if (w.willOverload(p)) {
+			System.out.println("This project will overload this poor worker.");
+		}
+		else if (p.isHelpful(w)) {
+			if (!assignedWorkerPool.contains(w))
+				assignedWorkerPool.add(w);
+			p.workers.add(w);
+		}
+		else {
+			System.out.println("Well, I don't know what to do for you. Sorry!");
+		}
 	}
 
 	public void unassign (Worker w, Project p) {
 		if (!p.workers.contains(w))
-			System.out.println("Worker not assigned to this project and will not be removed.");
+			System.out.println("This Worker is not assigned to this project and will not be removed.");
 		else {
 			p.workers.remove(w);
 			if (w.projectsAssignedTo.isEmpty()) {
 				assignedWorkerPool.remove(w);
 			}
-			// TODO: If the qualification requirements of an ACTIVE project are no longer met, that project is marked SUSPENDED. A PLANNED OR SUSPENDED project remains in that state.
+			if (p.getStatus() == ProjectStatus.ACTIVE && !p.missingQualifications().isEmpty()) {
+				p.setStatus(ProjectStatus.SUSPENDED);
+			}
 		}
 	}
 
-	public void unnasignAll (Worker w) {
-		for (int i = 0; i < w.projectsAssignedTo.size(); i++) {
-			Project project = w.projectsAssignedTo.get(i);
-			// TODO: Change the state of the affected projects as needed
-		}
+	public void unassignAll (Worker w) {
+		ArrayList<Project> former_projects = new ArrayList<Project> (w.projectsAssignedTo);
 		w.projectsAssignedTo.clear();
 		assignedWorkerPool.remove(w);
-		availableWorkerPool.add(w);
+		for (int i = 0; i < former_projects.size(); i++) {
+			if (former_projects.get(i).getStatus() == ProjectStatus.ACTIVE && !former_projects.get(i).missingQualifications().isEmpty()) {
+				former_projects.get(i).setStatus(ProjectStatus.SUSPENDED);
+			}
+		}
 	}
 
 	public void start (Project p) {
-		System.out.println("UNIMPLEMENTED");
+		switch (p.getStatus()) {
+		case PLANNED:
+		case SUSPENDED:
+			if (p.missingQualifications().isEmpty())
+				p.setStatus(ProjectStatus.ACTIVE);
+			else
+				System.out.println("Qualifications not met and project will not be started.");
+			break;
+		default:
+			System.out.println("Project already ACTIVE or FINISHED and cannot start.");
+			break;
+		}
 	}
 
 	public void finish (Project p) {
-		System.out.println("UNIMPLEMENTED");
+		switch (p.getStatus()) {
+		case ACTIVE:
+			p.setStatus(ProjectStatus.FINISHED);
+			for (int i = 0; i < p.workers.size(); i++) {
+				if (p.workers.get(i).projectsAssignedTo.size() <= 1) {
+					assignedWorkerPool.remove(p.workers.get(i));
+				}
+			}
+			p.workers.clear();
+			break;
+		case FINISHED:
+			System.out.println("Project already marked as FINISHED.");
+			break;
+		default:
+			System.out.println("Project SUSPENDED or PLANNED and is not ready to be finished.");
+			break;
+		}
 	}
 
-	public void createProject (String n, Qualification[] qs, ProjectSize size) {
-		System.out.println("UNIMPLEMENTED");
+	public void createProject (String n, ArrayList<Qualification> qs, ProjectSize size) {
+		Project created_project = new Project(n, size, ProjectStatus.PLANNED);
+		for (int i = 0; i < qs.size(); i++) {
+			created_project.requires.add(qs.get(i));
+		}
+		workingOn.add(created_project);
 	}
 }
